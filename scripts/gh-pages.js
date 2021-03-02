dc6ac648262ee8aa8675eed40cde4f9682c45e68
张雄军 <zxj@blibao.cn>
2021-03-02 16:21:48 +0800

    perf: 优化文档发布命令

--- scripts/gh-pages.js
+++ scripts/gh-pages.js
@@ -5,13 +5,15 @@
 // ------------------------------------------------------------------------------
 
 const ghpages = require('gh-pages');
+const date = new Date();
+const times = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}`;
 
 ghpages.publish(
   './docs/.vuepress/dist',
   {
     // 推送到远程分支，设置成 false 则只进行 commit
     push: true,
-    message: 'update docs and publish to github.io',
+    message: `update docs at \`${times}\``,
     function(err) {
       console.log(err);
     }
